636f6dfc215d62e5b06793ee32107c20b802dcdf
Serghei <lighsnow@gmail.com>
2020-09-23 22:30:41 +0300

    complete 3 homework

--- js/methods.js
+++ js/methods.js
@@ -1,3 +1,31 @@
 "use strict";
 
 
+const str = 'test';
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str);
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello World!";
+
+console.log(logg.slice(6));           //обрезает строку с определенного индекса
+console.log(logg.slice(-6));
+
+console.log(logg.substring(6, 12));   
+
+
+console.log(logg.substr(6, 6));       // сколько символов необходимо вырезать
+
+
+const num = 12.2;
+console.log(Math.round(num));    //округление до ближайшего целого
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
